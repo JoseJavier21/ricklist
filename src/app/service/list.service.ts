9a7f928f638ba3f6ccb77944b9fe5f84bae5496d
JoseJavier21 <92015037+JoseJavier21@users.noreply.github.com>
2024-04-03 10:31:10 +0200

    diseño de la celda, plantilla y prueba de api

---
+++ src/app/service/list.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';3
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Personaje } from '../model/personaje';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ListService {
+
+  url = "https://rickandmortyapi.com/api/character"
+
+  constructor(private http: HttpClient) { }
+
+  getDatos(): Observable<Personaje[]> {
+    return this.http.get<Personaje[]>(this.url)
+  }
+  
+}
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Personaje } from '../model/personaje';
+import { ListService } from '../service/list.service'; 
 
 @Component({
   selector: 'app-tab1',
@@ -8,5 +10,52 @@ import { Component } from '@angular/core';
 export class Tab1Page {
 
   constructor() {}
+
+  datos: Personaje[] = [
+    {
+      id: 1,
+      name: "Rick",
+      status: "Alive",
+      species: "Human",
+      type: "Main",
+      gender: "Male",
+      origin: {
+        name: "Earth",
+        url: "https://rickandmortyapi.com/api/location/1"
+      },
+      location: {
+        name: "Earth",
+        url: "https://rickandmortyapi.com/api/location/20"
+      },
+      image: "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
+      episode: [
+        "https://rickandmortyapi.com/api/episode/1",
+        "https://rickandmortyapi.com/api/episode/2",
+        "https://rickandmortyapi.com/api/episode/3",
+        "https://rickandmortyapi.com/api/episode/4",
+        "https://rickandmortyapi.com/api/episode/5",
+      ],
+      url: ""
+    }
+  ];
+
+  ionViewWillEnter() {
+    console.log('tab 1');
+  this.obtenerDatos();
+  }
+
+  obtenerDatos() {
+    console.log('obtenerDatos');
+/*       this.apiservice.getDatos().subscribe({
+      next: res => {
+        this.datos = res;
+        console.log(res);
+      },
+      error: error => {
+        console.error('Ha ocurrido un error', error);
+      }
+    }); */
+  }
+ 
 
 }
